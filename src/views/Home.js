09a607550be1417665fdd81d3a06fe0ad3f24497
fabianferno <fabianferno@gmail.com>
2021-01-28 11:51:18 +0530

    Add Tesimonials. Remove warnings

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 // import sections
 import Hero from "../components/sections/Hero";
 import FeaturesTiles from "../components/sections/FeaturesTiles";
-import FeaturesSplit from "../components/sections/FeaturesSplit";
+// import FeaturesSplit from "../components/sections/FeaturesSplit";
 import Testimonial from "../components/sections/Testimonial";
 import Cta from "../components/sections/Cta";
 import FluidAnimation from "react-fluid-animation";
@@ -31,8 +31,8 @@ const Home = () => {
         topDivider
         imageFill
         className="illustration-section-02"
-      />
-      <Testimonial topDivider /> */}
+      />*/}
+      <Testimonial topDivider /> 
       <Cta split />
     </>
   );
