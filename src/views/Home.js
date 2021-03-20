ba0d3900dd7616c3407faa1e2d811be8377def23
Fabian Ferno <fabianferno@gmail.com>
2021-03-20 11:40:44 +0530

    Landing Update

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,6 +4,7 @@ import Services from "../components/sections/Services";
 import FluidAnimation from "react-fluid-animation";
 import { motion } from "framer-motion";
 import heroImg from "../assets/images/hero.jpg";
+import logo from "../assets/images/logo.svg";
 
 const easing = [0.6, -0.05, 0.01, 0.99];
 const fade = {
@@ -14,6 +15,19 @@ const fade = {
     opacity: 1,
     transition: {
       duration: 1,
+      ease: easing,
+    },
+  },
+};
+
+const fade1 = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 3,
       ease: easing,
     },
   },
@@ -37,7 +51,7 @@ const Home = () => {
             style={{ height: "100vh", width: "100vw" }}
           />
 
-          <div
+          <motion.div
             style={{
               position: "absolute",
               top: "20vh",
@@ -46,30 +60,48 @@ const Home = () => {
               marginLeft: "auto",
               marginRight: "auto",
             }}
-            className="container p-4"
+            className="container p-5"
+            variants={fade1}
           >
             <div
-              drag
-              dragConstraints={{
-                top: -10,
-                left: -0,
-                right: 0,
-                bottom: 10,
-              }}
               className="d-md-flex d-block justify-content-around align-items-center col-md-12"
               style={{
-                padding: "30px",
+                padding: "50px",
                 background: "#222222",
               }}
             >
-              <img
-                className="col-md-4 img-fluid"
-                alt="kathir-kreation-logo"
-                src={heroImg}
-              />
+              <motion.div
+                className="m-2 p-3 col-md-4 "
+                variants={fade}
+                animate={{
+                  scale: [1.15, 1, 1.15, 1, 1.15],
+                  rotate: [-90, 0],
+                }}
+                transition={{
+                  duration: 4,
+                  ease: "easeInOut",
+                  times: [0, 0.2, 0.5, 0.8, 1],
+                  // loop: Infinity,
+                }}
+                drag
+                dragConstraints={{
+                  top: -10,
+                  left: -10,
+                  right: 10,
+                  bottom: 10,
+                }}
+              >
+                <img
+                  className="img-fluid"
+                  alt="kathir-kreation-logo"
+                  src={logo}
+                  style={{ pointerEvents: "none" }}
+                />
+              </motion.div>
+
               <div className="d-block pl-md-4 pl-0 col-md-8">
                 <h1 className="text-white">We care for your creativity</h1>
-                <p className="text-white">
+                <p className="text-white text-justify">
                   Our company is the persona of influence which forges brands
                   that are developing to accomplish their full potential and
                   help them exceed their unimaginable boundaries. We develop
@@ -80,7 +112,7 @@ const Home = () => {
                 </p>
               </div>
             </div>
-          </div>
+          </motion.div>
           <div style={{ paddingTop: "500px" }} className="d-md-none d-block" />
           <section className="illustration-section-01 pt-5">
             <div className="container text-center">
